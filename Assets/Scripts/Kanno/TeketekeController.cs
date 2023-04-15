657b0501fa452a7fb4479a7cfc9ffa8bcbbddf50
HirotoKanno <a20dc060@dhw.ac.jp>
2023-04-16 02:52:33 +0900

    Teketeke

---
+++ Assets/Scripts/Kanno/TeketekeController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hanako
+{
+    public class TeketekeController : MonoBehaviour
+    {
+        [SerializeField] private float speed;//プレイヤーの移動速度
+        [SerializeField] private float maxY, minY; //移動範囲の制限
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MovePlayer(); //プレイヤーを動かすメソッドを呼び出す
+        }
+
+        void MovePlayer()
+        {
+            //もし上矢印キーが押されたら
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                Vector3 playerPos = transform.position; //Vector3型のplayerPosに現在の位置情報を格納
+                playerPos.y += speed * Time.deltaTime; //y座標にspeedを加算
+
+                //もしplayerPosのY座標がmaxY（最大Y座標）より大きくなったら
+                if (maxY < playerPos.y)
+                {
+                    playerPos.y = maxY; //PlayerPosのY座標にmaxYを代入
+                }
+
+                transform.position = playerPos; //現在の位置情報に反映させる
+            }
+
+            
+            else if (Input.GetAxisRaw("Vertical") < 0) //もし下矢印キーが押されたら
+            {
+                Vector3 playerPos = transform.position;
+                playerPos.y -= speed * Time.deltaTime;
+
+                if (minY > playerPos.y)
+                {
+                    playerPos.y = minY;
+                }
+
+                transform.position = playerPos;
+            }
+        }
+    }
+}
