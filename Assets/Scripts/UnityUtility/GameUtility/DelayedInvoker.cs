a945884ac64f51f289606ceb2a8749f0a4476b4b
Ray <blokymose58@gmail.com>
2023-06-03 16:16:21 +0900

    Test animation: summon

--- Assets/Scripts/UnityUtility/GameUtility/DelayedInvoker.cs
+++ Assets/Scripts/UnityUtility/GameUtility/DelayedInvoker.cs
@@ -1,7 +1,10 @@
+using Encore.Utility;
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityUtility;
 
 namespace Tsutaeru
 {
@@ -30,8 +33,33 @@ namespace Tsutaeru
             }
         }
 
+        [SerializeField, HorizontalGroup("Auto")]
+        bool isAutoInvoke = true;
+
+        [SerializeField,ShowIf(nameof(isAutoInvoke)), HorizontalGroup("Auto"), LabelWidth(0.1f)]
+        UnityInitialMethod invokeOn = UnityInitialMethod.Awake;
+
+
         [SerializeField]
         List<DelayedEvent> events = new();
+
+        private void Awake()
+        {
+            if (isAutoInvoke && invokeOn == UnityInitialMethod.Awake)
+                InvokeAll();
+        }
+
+        private void OnEnable()
+        {
+            if (isAutoInvoke && invokeOn == UnityInitialMethod.OnEnable)
+                InvokeAll();
+        }
+
+        private void Start()
+        {
+            if (isAutoInvoke && invokeOn == UnityInitialMethod.Start)
+                InvokeAll();
+        }
 
         public void Invoke(string eventName)
         {
---
+++ Assets/Scripts/UnityUtility/GameUtility/SpriteRendererEditor.cs
@@ -0,0 +1,69 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityUtility;
+
+namespace Hanako
+{
+    public class SpriteRendererEditor : MonoBehaviour
+    {
+        public List<SpriteRenderer> srs = new();
+
+        [Button("Get All SRs")]
+        public void GetAllSpriteRenderersInChildren()
+        {
+            srs.AddRange(transform.GetComponentsInChildren<SpriteRenderer>());
+        }
+
+        public void BeTransparent(float duration)
+        {
+            ChangeAlpha(duration, 0f);
+        }
+
+        public void BeOpaque(float duration)
+        {
+            ChangeAlpha(duration, 1f);
+        }
+
+        /// <summary>
+        /// The parameters are duration, followed by a semi-colon, then the alpha value<br></br>
+        /// Example: "0.15;1"
+        /// </summary>
+        public void ChangeAlpha(string parameters)
+        {
+            var parametersSplitted = parameters.Split(';');
+            if (float.TryParse(parametersSplitted[0], out float duration))
+            {
+                if (float.TryParse(parametersSplitted[1], out float alpha))
+                {
+                    ChangeAlpha(duration, alpha);
+                }
+            }
+
+            Debug.LogWarning("Failed to change alpha because the string format is wrong; it should be 'duration;alpha'");
+        }
+
+        public void ChangeAlpha(float duration, float alpha)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Delay());
+            IEnumerator Delay()
+            {
+                var curve = AnimationCurve.EaseInOut(0, srs[0].color.a, duration, alpha);
+                var time = 0f;
+                while (true)
+                {
+                    foreach (var sr in srs)
+                    {
+                        sr.color = sr.color.ChangeAlpha(curve.Evaluate(time));
+                    }
+                    time += Time.deltaTime;
+
+                    if (time > duration) break;
+                    yield return null;
+                }
+            }
+        }
+    }
+}
